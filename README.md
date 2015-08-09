a53f2ba136de668e9d38d73da97f90b223288b19
Brian Chuk <brian_chuk@yahoo.com>
2015-08-09 03:12:30 -0400

    Splash screen complete!

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # resources
 Miscellaneous logos, pictures, icons for Owl
+
+
+https://coolors.co/app/986d5f-1c1e20-232325-242628-363636
+
+Main font: Montserrat
+
+Supporting font: Museo Sans
---
+++ layouts_app/OwlLayouts/app/src/main/java/com/battlehack_nyc/root/owllayouts/TestActivity.java
@@ -0,0 +1,45 @@
+package com.battlehack_nyc.root.owllayouts;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        TextView tx = (TextView)findViewById(R.id.splash);
+
+        Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/Montserrat-Light.otf");
+
+        tx.setTypeface(custom_font);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_test, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
